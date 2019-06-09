b46955fe5f8a84e8458b4c146c653dfd38b45c30
Sergey <kochkares@gmail.com>
2019-06-09 18:47:45 +0300

    ver 1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.тижденьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.оголошенняToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewLesson = new System.Windows.Forms.DataGridView();
+            this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLesson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.тижденьToolStripMenuItem,
+            this.оголошенняToolStripMenuItem,
+            this.вихідToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStripMain.Size = new System.Drawing.Size(1048, 24);
+            this.menuStripMain.TabIndex = 0;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // тижденьToolStripMenuItem
+            // 
+            this.тижденьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.тижденьToolStripMenuItem.Name = "тижденьToolStripMenuItem";
+            this.тижденьToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.тижденьToolStripMenuItem.Text = "Тиждень";
+            this.тижденьToolStripMenuItem.Click += new System.EventHandler(this.тижденьToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Перший",
+            "Другий"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.Text = "Перший";
+            this.toolStripComboBox1.TextChanged += new System.EventHandler(this.toolStripComboBox1Changed);
+            // 
+            // оголошенняToolStripMenuItem
+            // 
+            this.оголошенняToolStripMenuItem.Name = "оголошенняToolStripMenuItem";
+            this.оголошенняToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.оголошенняToolStripMenuItem.Text = "Оголошення";
+            this.оголошенняToolStripMenuItem.Click += new System.EventHandler(this.оголошенняToolStripMenuItem_Click);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
+            // 
+            // dataGridViewLesson
+            // 
+            this.dataGridViewLesson.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridViewLesson.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewLesson.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewLesson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewLesson.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewLesson.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewLesson.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridViewLesson.Location = new System.Drawing.Point(0, 24);
+            this.dataGridViewLesson.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewLesson.Name = "dataGridViewLesson";
+            this.dataGridViewLesson.RowTemplate.Height = 24;
+            this.dataGridViewLesson.RowTemplate.ReadOnly = true;
+            this.dataGridViewLesson.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewLesson.Size = new System.Drawing.Size(1048, 212);
+            this.dataGridViewLesson.TabIndex = 1;
+            this.dataGridViewLesson.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewLesson_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 236);
+            this.Controls.Add(this.dataGridViewLesson);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "InfoSystem";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLesson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridViewLesson;
+        private System.Windows.Forms.ToolStripMenuItem тижденьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem оголошенняToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadTable("default");
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void loadTable(string param)
+        {
+            string myConnection = "server=web113.default-host.net;Port=3306;uid=daleth_max;pwd=huE96m5VHbC3;database=daleth_warehouse";
+            MySqlConnection myConnect = new MySqlConnection(myConnection);
+
+            string selectQuery = "";
+
+            try
+            {
+                myConnect.Open();
+
+                if (param == "Перший") {
+                    selectQuery =
+                    "SELECT lesson_times, lesson_Monday, lesson_Tuesday, lesson_Wednesday, lesson_Thursday, lesson_Friday FROM daleth_warehouse.info_system WHERE study_week = 1;";
+                }
+                if (param == "Другий") {
+                    selectQuery =
+                    "SELECT lesson_times, lesson_Monday, lesson_Tuesday, lesson_Wednesday, lesson_Thursday, lesson_Friday FROM daleth_warehouse.info_system WHERE study_week = 2;";
+                }
+
+                if (param == "default")
+                {
+                    selectQuery =
+                    "SELECT lesson_times, lesson_Monday, lesson_Tuesday, lesson_Wednesday, lesson_Thursday, lesson_Friday FROM daleth_warehouse.info_system WHERE study_week = 2;";
+                }
+
+                MySqlCommand command = new MySqlCommand(selectQuery, myConnect);
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+
+                dataAdapter.SelectCommand = command;
+                DataTable dTable = new DataTable();
+
+                dataAdapter.Fill(dTable);
+
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dTable;
+                dataGridViewLesson.DataSource = bSource;
+                dataAdapter.Update(dTable);
+
+                //dataGridViewLesson.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+                //dataGridViewLesson.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+                dataGridViewLesson.Columns[0].HeaderText = "Часи занять";
+                dataGridViewLesson.Columns[0].Width = 100;
+                dataGridViewLesson.Columns[1].HeaderText = "Понеділок";
+                dataGridViewLesson.Columns[1].Width = 250;
+                dataGridViewLesson.Columns[2].HeaderText = "Вівторок";
+                dataGridViewLesson.Columns[2].Width = 250;
+                dataGridViewLesson.Columns[3].HeaderText = "Середа";
+                dataGridViewLesson.Columns[3].Width = 250;
+                dataGridViewLesson.Columns[4].HeaderText = "Четвер";
+                dataGridViewLesson.Columns[4].Width = 250;
+                dataGridViewLesson.Columns[5].HeaderText = "П'ятниця";
+                dataGridViewLesson.Columns[5].Width = 250;
+
+                dataGridViewLesson.Rows[1].Height = 40;
+
+
+                myConnect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void loadMessage(string param)
+        {
+            string myConnection = "server=web113.default-host.net;Port=3306;uid=daleth_max;pwd=huE96m5VHbC3;database=daleth_warehouse";
+            MySqlConnection myConnect = new MySqlConnection(myConnection);
+
+            string selectQuery = "";
+
+            try
+            {
+                myConnect.Open();
+
+                if (param == "Перший")
+                {
+                    selectQuery =
+                    "SELECT lesson_Monday FROM daleth_warehouse.info_system WHERE study_week = 3;";
+                }
+                if (param == "Другий")
+                {
+                    selectQuery =
+                    "SELECT lesson_Monday FROM daleth_warehouse.info_system WHERE study_week = 4;";
+                }
+
+                MySqlCommand command = new MySqlCommand(selectQuery, myConnect);
+                
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                da.SelectCommand = command;
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    MessageBox.Show(dr["lesson_Monday"].ToString());
+                }
+
+                myConnect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void dataGridViewLesson_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        void toolStripComboBox1Changed(object sender, EventArgs e)
+        {
+            loadTable(this.toolStripComboBox1.Text);
+            //MessageBox.Show(this.toolStripComboBox1.Text);
+        }
+
+        private void оголошенняToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loadMessage(this.toolStripComboBox1.Text);
+        }
+
+        private void тижденьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
